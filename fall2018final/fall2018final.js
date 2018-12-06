20ab242fba215613036dca39db77f57304de9352
Mackenzie <mmk3054@rit.edu>
2018-12-06 15:14:31 -0500

    Save

---
+++ fall2018final/fall2018final.js
@@ -0,0 +1,25 @@
+'use strict';
+
+$(".top").click(function() {
+    $(this).next(".list").slideToggle('slow');
+});
+
+
+let verse = ("article1.txt")  // sets default verse element
+
+$(".story").val(verse); // changes menu option to default
+$("#story").load(verse);   // retrieves only default element
+
+
+$(".story").click(function() { //on change, when a new menu item is selected do this
+verse = ("article2.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
+    
+$(".story").load(verse); //upload the new verse and replace the old one
+});
+
+
+$(".story").click(function() { //on change, when a new menu item is selected do this
+verse = ("article1.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
+    
+$(".story").load(verse); //upload the new verse and replace the old one
+});
