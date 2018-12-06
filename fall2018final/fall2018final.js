6e62d6478d03358eb81291a2bef48d1f771e1997
Mackenzie <mmk3054@rit.edu>
2018-12-06 15:19:50 -0500

    done

--- fall2018final/fall2018final.js
+++ fall2018final/fall2018final.js
@@ -8,18 +8,18 @@ $(".top").click(function() {
 let verse = ("article1.txt")  // sets default verse element
 
 $(".story").val(verse); // changes menu option to default
-$("#story").load(verse);   // retrieves only default element
+$(".h2").load(verse);   // retrieves only default element
 
 
 $(".story").click(function() { //on change, when a new menu item is selected do this
 verse = ("article2.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
     
-$(".story").load(verse); //upload the new verse and replace the old one
+$(".h2").load(verse); //upload the new verse and replace the old one
 });
 
 
 $(".story").click(function() { //on change, when a new menu item is selected do this
 verse = ("article1.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
     
-$(".story").load(verse); //upload the new verse and replace the old one
+$(".h2").load(verse); //upload the new verse and replace the old one
 });
